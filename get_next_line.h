48af4270ebe29cc83d509f002d30e168aeaecaf1
mhufflep <mhufflep@student.21-school.ru>
2021-01-12 20:31:59 +0300

    get_next_line v1.0

---
+++ get_next_line.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhufflep <mhufflep@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/06 05:07:30 by mhufflep          #+#    #+#             */
+/*   Updated: 2020/11/20 02:10:03 by mhufflep         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+typedef struct		s_thread
+{
+	int				fd;
+	char			*buf;
+	struct s_thread *next;
+}					t_thread;
+
+t_thread			*search_thread(int fd, t_thread **threads);
+t_thread			*push_thread(int fd, t_thread **threads);
+void				delete_element(t_thread **threads, t_thread *current);
+void				reallocate(t_thread *current, char *arg1, char *arg2);
+void				*ft_memcpy(void *dst, const void *src, size_t n);
+void				clean_buffer(char *buf);
+char				*ft_strjoin(char *s1, char *s2);
+int					get_next_line(int fd, char **line);
+
+#endif
